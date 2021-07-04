0558006831297be4e64058825bbd179b99ada457
Vardan1995 <3110249@gmail.com>
2021-07-04 19:17:54 +0400

    demo

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/Vardan1995/go_api/database"
+	"github.com/Vardan1995/go_api/router"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
 
 func main() {
-	fmt.Println("start")
+	app := fiber.New()
+	app.Use(cors.New())
+
+	database.ConnectDB()
+
+	router.SetupRoutes(app)
+	log.Fatal(app.Listen(":3000"))
+
+	defer database.DB.Close()
 }
